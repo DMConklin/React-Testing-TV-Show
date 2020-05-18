e33539f7202f77b6b70af70ba7d76804350dcf92
Darrell-Conklin <dmconklin84@gmail.com>
2020-05-18 18:32:49 -0500

    running render and getByText tests but still have act() error

---
+++ src/App.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, wait } from '@testing-library/react';
+import App from './App';
+import { fetchShow as mockFetchShow } from './api/fetchShow';
+import { act } from 'react-dom/test-utils';
+
+jest.mock('./api/fetchShow');
+
+const mockData = {
+    data: {
+        _embedded: {
+            episodes: [
+                {
+                    id: 1234,
+                    image: {
+                        medium: 'https://www.tutorialgateway.org/wp-content/uploads/JavaScript-Date-Now-Function-1-1024x700.png'
+                    },
+                    name: 'name',
+                    season: 1,
+                    episode: 1,
+                    summary: 'summary',
+                    runtime: 60
+                }
+            ]
+        },
+        name: 'name',
+        image: {
+            original: 'google.com'
+        },
+        summary: 'summary'
+    }
+}
+
+test('Does App render', async () => {
+        mockFetchShow.mockResolvedValueOnce(mockData);
+        const {getByText} = await render(<App />);
+        getByText(/Select a season/i);
+})
+
+
